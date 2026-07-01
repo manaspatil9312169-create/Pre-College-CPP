2a63e3c6111310f3c83cdaa6d57fe23a2646887f
Blaze Phoenix <manaspatil9312169@gmail.com>
2026-07-01 13:25:07 +0530

    Create 01_input_and_output.cpp

---
+++ 01_input_and_output.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
